b8a82fd91af98b032b63551de8f90e1a677ffe87
lucrezia sabba <lucrezia.sabba@icloud.com>
2020-03-24 15:35:02 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleEsercizioStringhe/Program.cs
@@ -0,0 +1,36 @@
+﻿using EserciziStringhe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEsercizioStringhe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prova il primo metodo");
+            Console.WriteLine("Inserisci la stringa: ");
+            string s1 = Console.ReadLine();
+            Console.WriteLine("Inserisci la stringa: ");
+            string s2 = Console.ReadLine();
+            bool risultato = Metodi.AreEquals(s1, s2);
+            Console.WriteLine("La risposta è: "+risultato);
+
+            Console.WriteLine("Prova il secondo metodo");
+            Console.WriteLine("Inserisci la stringa: ");
+            string s = Console.ReadLine();
+            bool risultato2 = Metodi.ContainsDoubleChar(s);
+            Console.WriteLine("la risposta è: "+risultato2);
+
+            Console.WriteLine("Prova il terzo metodo");
+            Console.WriteLine("Inserisci la stringa:  ");
+            string s3 = Console.ReadLine();
+            string risposta2 = Metodi.Convert(s3);
+            Console.WriteLine(risposta2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EserciziStringhe/Metodi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace EserciziStringhe
+{
+    public class Metodi
+    {
+        public static bool AreEquals(string s1, string s2)
+        {
+            if (s1 == s2)
+                return true;
+            else
+                return false;
+        }
+        public static bool ContainsDoubleChar(string s)
+        {
+            if (s.Contains("A") || s.Contains("E") || s.Contains("I") || s.Contains("O") || s.Contains("U"))
+                return true;
+            else
+                return false;
+        }
+        public static string Convert(string s3)
+        {
+            string titleCase = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(s3);
+            return (titleCase);
+        }
+    }
+}
